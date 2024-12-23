784c3cdfa4824073c37aac08b8e0a11a6b7974aa
terchem <63562115+terchem@users.noreply.github.com>
2024-12-23 13:22:36 +0100

    Update SMILES-TO-TOPO.py

--- SMILES-TO-TOPO.py
+++ SMILES-TO-TOPO.py
@@ -1,14 +1,14 @@
 from rdkit import Chem
 from rdkit.Chem import Draw
 
-# Define a SMILES string
+
 smiles = "CC(O)CCC(C)(C)O"  # Ethanol
 
-# Convert SMILES to an RDKit molecule object
+
 mol = Chem.MolFromSmiles(smiles)
 # Check if the molecule object is valid
 if mol:
-    # Render and save the molecule as an image
+    
     img = Draw.MolToImage(mol)
     img.show()  # Opens the image in the default viewer
 else:
